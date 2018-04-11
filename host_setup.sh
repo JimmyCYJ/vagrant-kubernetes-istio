16ac676e8445474b3b8a80e2a376a9f1b170dbb4
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-04-11 14:41:46 -0700

    Update host_setup.sh

--- host_setup.sh
+++ host_setup.sh
@@ -2,11 +2,11 @@
 
 # Install vagrant
 echo "Install vagrant"
-sudo apt-get install vagrant -y
+sudo apt-get --quiet -y install vagrant 
 
 # Install virtualbox
 echo "Install virtualbox"
-sudo apt-get install -y libpng16-16 libssl1.0.2 libvpx4
+sudo apt-get install --quiet -y libpng16-16 libssl1.0.2 libvpx4
 curl -L https://download.virtualbox.org/virtualbox/5.2.8/virtualbox-5.2_5.2.8-121009~Ubuntu~xenial_amd64.deb --output virtualbox.deb
 sudo dpkg -i virtualbox.deb -y
 sudo apt-get install -f
@@ -17,6 +17,15 @@ cd $ISTIO
 mkdir flix-tech
 cd flix-tech
 vagrant init flixtech/kubernetes
+
+# Modify vagrant to run in non-sudo mode
+echo "Modify vagrant to run in non-sudo mode"
+echo "Run $ls -al $ISTIO/flix-tech/.vagrant/ to find all files/folders that require modification."
+
+sudo chown $USER:$(id -gn) .vagrant/machines/
+sudo chown $USER:$(id -gn) .vagrant/machines/default/
+sudo chown $USER:$(id -gn) .vagrant/machines/default/virtualbox/
+sudo chown $USER:$(id -gn) .vagrant/machines/default/virtualbox/vagrant_cwd
 
 # Set route rule to access VM
 echo "Set route rule to access VM"

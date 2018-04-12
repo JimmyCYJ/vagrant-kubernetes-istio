3e82c3a5f976ba46245c1b112996574128d52c86
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-04-12 11:38:19 -0700

    Create test_setup.sh

---
+++ RunTestOnVm/test_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Start vagrant if not already started
+vagrant up --provider virtualbox
+
+# Setup HUB and TAG to talk to insecure local registry on VM.
+HUB=10.10.0.2:5000 
+TAG=latest 
+
+# Make and Push images to insecure local registry on VM.
+cd $ISTIO/istio
+make docker HUB=10.10.0.2:5000 TAG=latest
+make push HUB=10.10.0.2:5000 TAG=latest
+
+# Verify images are pushed in repository.
+echo "Check images present in repositories"
+curl 10.10.0.2:5000/v2/_catalog -v
+
+

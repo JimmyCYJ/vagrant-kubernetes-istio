b35565562a74608b7066de0235a3a009c67c904e
gargnupur <37600866+gargnupur@users.noreply.github.com>
2018-04-12 10:13:06 -0700

    Update and rename run_test_setup.sh to test_setup.sh

--- RunTestOnHost/test_setup.sh
+++ RunTestOnHost/test_setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+# Setup HUB and TAG to talk to insecure local registry on VM.
 HUB=10.10.0.2:5000 
 TAG=latest 
+
+# Make and Push images to insecure local registry on VM.
 cd $ISTIO/istio
 make docker HUB=10.10.0.2:5000 TAG=latest
 make push HUB=10.10.0.2:5000 TAG=latest
+
+# Verify images are pushed in repository.
 echo "Check images present in repositories"
 curl 10.10.0.2:5000/v2/_catalog -v
 

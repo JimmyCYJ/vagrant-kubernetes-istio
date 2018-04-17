4f656a2f604028511e9cb62643ffa35fb3a988a7
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-04-17 15:11:58 -0700

    Update vm_setup.sh

--- RunTestOnVm/vm_setup.sh
+++ RunTestOnVm/vm_setup.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# Set up Istio
-echo "export GOPATH=/home/vagrant/go" >> $HOME/.profile
-echo "export PATH=$PATH:$GOPATH/bin" >> $HOME/.profile
-echo "export ISTIO=$GOPATH/src/istio.io" >> $HOME/.profile
-echo "export HUB=localhost:5000" >> $HOME/.profile
-echo "export TAG=latest" >> $HOME/.profile
-source $HOME/.profile
-
 mkdir -p $ISTIO
 
 # Copy kube config

798bee51424276087753016106a3d884bd5ac042
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-04-17 14:47:28 -0700

    Update and rename vm_setup.sh to vm_bootstrap.sh

--- RunTestOnVm/vm_bootstrap.sh
+++ RunTestOnVm/vm_bootstrap.sh
@@ -13,16 +13,6 @@ sudo mv go /usr/local
 echo "Install git"
 sudo apt-get --quiet -y install git
 
-# Set up Istio
-echo "export GOPATH=/home/vagrant/go" >> /.profile
-echo "export PATH=$PATH:$GOPATH/bin" >> $HOME/.profile
-echo "export ISTIO=$GOPATH/src/istio.io" >> $HOME/.profile
-echo "export HUB=localhost:5000" >> $HOME/.profile
-echo "export TAG=latest" >> $HOME/.profile
-source $HOME/.profile
-
-mkdir -p $ISTIO
-
 # Setting up kubernetest Cluster on VM for Istio Tests.
 echo "Adding priviledges to kubernetes cluster..."
 sudo sed -i 's/ExecStart=\/usr\/bin\/hyperkube kubelet/ExecStart=\/usr\/bin\/hyperkube kubelet --allow-privileged=true/' /etc/systemd/system/kubelet.service
@@ -35,7 +25,3 @@ sudo systemctl daemon-reload
 sudo systemctl stop kube-apiserver
 sudo systemctl restart kube-apiserver
 
-# Copy kube config
-echo "Copy kube config file to ~/.kube/config"
-mkdir $HOME/.kube
-cp /etc/kubeconfig.yml $HOME/.kube/config

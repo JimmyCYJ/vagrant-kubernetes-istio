eefe998960fd175177f8e86e0985361cbfc2a051
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-04-17 12:57:50 -0700

    Update test_setup.sh

--- RunTestOnVm/test_setup.sh
+++ RunTestOnVm/test_setup.sh
@@ -6,7 +6,9 @@ TAG=latest
 
 # Make and Push images to insecure local registry on VM.
 cd $ISTIO/istio
+echo "Build docker images."
 make docker HUB=10.10.0.2:5000 TAG=latest
+echo "Push docker images to local registry in VM."
 make push HUB=10.10.0.2:5000 TAG=latest
 
 # Verify images are pushed in repository.

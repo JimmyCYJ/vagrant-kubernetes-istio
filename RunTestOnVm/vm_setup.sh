254604add68f8e1a2fe546d5d5f43508b1efff01
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-04-17 13:54:51 -0700

    Update vm_setup.sh

--- RunTestOnVm/vm_setup.sh
+++ RunTestOnVm/vm_setup.sh
@@ -14,12 +14,14 @@ echo "Install git"
 sudo apt-get --quiet -y install git
 
 # Set up Istio
-echo "export GOPATH=/usr/local/go" >> $HOME/.profile
+echo "export GOPATH=~/go" >> $HOME/.profile
 echo "export PATH=$PATH:$GOPATH/bin" >> $HOME/.profile
-echo "export ISTIO=/istio" >> $HOME/.profile
+echo "export ISTIO=$GOPATH/src/istio.io" >> $HOME/.profile
 echo "export HUB=localhost:5000" >> $HOME/.profile
 echo "export TAG=latest" >> $HOME/.profile
 source $HOME/.profile
+
+mkdir -p $ISTIO
 
 # Setting up kubernetest Cluster on VM for Istio Tests.
 echo "Adding priviledges to kubernetes cluster..."

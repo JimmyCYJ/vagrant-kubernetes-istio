487eb9a3afe2d7a6c5ad21fea5ec91abcf85c7a5
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-04-17 13:09:11 -0700

    Update host_setup.sh

--- RunTestOnVm/host_setup.sh
+++ RunTestOnVm/host_setup.sh
@@ -8,6 +8,7 @@ vagrant up --provider virtualbox
 echo "Adding insecure registry to docker daemon in host vm..."
 echo "You old docker daemon file can be found at /lib/systemd/system/docker.service_old"
 sudo cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service_old
+echo "sudo sed -i 's/ExecStart=\/usr\/bin\/dockerd -H fd:\/\//ExecStart=\/usr\/bin\/dockerd -H fd:\/\/ --insecure-registry 10.10.0.2:5000/' /lib/systemd/system/docker.service"
 sudo sed -i 's/ExecStart=\/usr\/bin\/dockerd -H fd:\/\//ExecStart=\/usr\/bin\/dockerd -H fd:\/\/ --insecure-registry 10.10.0.2:5000/' /lib/systemd/system/docker.service
 sudo systemctl daemon-reload
 sudo systemctl restart docker
@@ -15,3 +16,5 @@ sudo systemctl restart docker
 # Set route rule to access VM
 echo "Set route rule to access VM"
 sudo ip route add 10.0.0.0/24 via 10.10.0.2
+
+echo "Please run docker info and make sure insecure registry address is updated to 10.10.0.2:5000"
